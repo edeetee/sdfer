a6b6eac681f3c2f75f98bfa386594c14e556b91f
Edward Taylor <edeetee@gmail.com>
2024-03-13 02:32:20 -0700

    Update img.rs

--- src/img.rs
+++ src/img.rs
@@ -83,7 +83,7 @@ impl<T, Storage: AsRef<[T]>> Image2d<T, Storage> {
     }
 
     pub fn storage(&self) -> &Storage {
-        self.storage
+        &self.data
     }
 
     pub fn width(&self) -> usize {
